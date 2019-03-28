fa456e5c4c879365ee3abee663183b9863a752ca
Tomas Gallucci <professortom@gmail.com>
2019-03-28 11:31:08 -0500

    Initial Commit

---
+++ Image Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Image Viewer
+//
+//  Created by Tomas Gallucci on 3/28/19.
+//  Copyright © 2019 Tomas Gallucci. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
