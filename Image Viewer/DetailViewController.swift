a68e22605b637996f30d6e8c227dd59a09b4781e
Tomas Gallucci <professortom@gmail.com>
2019-03-28 12:33:07 -0500

    add view controllers for each side of the SplitView View Controller

---
+++ Image Viewer/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Image Viewer
+//
+//  Created by Tomas Gallucci on 3/28/19.
+//  Copyright © 2019 Tomas Gallucci. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Image Viewer/SourceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SourceViewController.swift
+//  Image Viewer
+//
+//  Created by Tomas Gallucci on 3/28/19.
+//  Copyright © 2019 Tomas Gallucci. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
