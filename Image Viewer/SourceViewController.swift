f65073e0083e8790c3c600448c52179a6baf6b69
Tomas Gallucci <professortom@gmail.com>
2019-03-28 13:11:29 -0500

    scaffolding for table data

--- Image Viewer/SourceViewController.swift
+++ Image Viewer/SourceViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class SourceViewController: NSViewController {
+class SourceViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet var tableView: NSTableView!
     
@@ -16,6 +16,17 @@ class SourceViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil}
+        vw.textField?.stringValue = "Hello World!"
+        
+        return vw
     }
     
 }
