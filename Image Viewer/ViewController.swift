d1dd9a8316ee0b2baf3187dcc4d289821822eb57
Tomas Gallucci <professortom@gmail.com>
2019-04-07 09:59:09 -0500

    added share button

--- Image Viewer/ViewController.swift
+++ Image Viewer/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: NSSplitViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func sharedClick(_ sender: NSView) {
+        guard let detail = children[1] as? DetailViewController else {return}
+        guard let image = detail.imageView.image else {return}
+        
+        let picker = NSSharingServicePicker(items: [image])
+        picker.show(relativeTo: .zero, of: sender, preferredEdge: .minY)
+    }
 
     override var representedObject: Any? {
         didSet {
